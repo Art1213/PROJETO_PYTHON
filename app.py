c7462f2a7d3a192a6828df08fb87bdd6f4678010
Art1213 <artur.vilhena13@gmail.com>
2025-10-13 14:15:39 -0300

    Corrige versão do kubectl para v1.29.0

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"message": "Aplicação CI/CD funcionando com sucesso!"}
+    return {"message": "Aplicação Python funcionando com sucesso!"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
